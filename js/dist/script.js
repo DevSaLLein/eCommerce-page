b1b9d6503380351ad351594611a22ba344864a8e
DevSaLLein <isaac.andrade3@aluno.ce.gov.br>
2024-07-28 19:49:52 -0300

    styles e scripts organizados

--- js/dist/script.js
+++ js/dist/script.js
@@ -1,21 +1,6 @@
+import { cartMenu } from "./cartMenu.js";
+import { mobileMenu } from "./mobileMenu.js";
 import { quantityProduct } from "./quantityProduct.js";
-window.addEventListener('load', () => {
-    const menus = () => {
-        const menuButton = document.querySelector("[rel='js-menu-button']");
-        const menuCart = document.querySelector("[rel='js-menu-cart-button']");
-        menuButton?.addEventListener('click', handleClickMenuMobile);
-        menuCart?.addEventListener('click', handleClickMenuCart);
-    };
-    const handleClickMenuMobile = () => {
-        const menuDropDown = document.querySelector("[rel='js-menu-dropdown']");
-        const backdropEffect = document.querySelector("[rel='js-menu-dropdown-blur-effect']");
-        menuDropDown?.classList.toggle('active-menu');
-        backdropEffect?.classList.toggle('blur-menu-mobile-active');
-    };
-    const handleClickMenuCart = () => {
-        const modalCart = document.querySelector("[rel='js-menu-cart-modal']");
-        modalCart?.classList.toggle('cart-menu-active');
-    };
-    menus();
-    quantityProduct();
-});
+quantityProduct();
+mobileMenu();
+cartMenu();
--- js/script.ts
+++ js/script.ts
@@ -1,29 +1,8 @@
+import { cartMenu } from "./cartMenu.js";
+import { mobileMenu } from "./mobileMenu.js";
 import { quantityProduct } from "./quantityProduct.js";
 
-window.addEventListener('load', (): void => {
    
-    const menus = (): void => {
-        const menuButton: HTMLBodyElement | null = document.querySelector("[rel='js-menu-button']");
-        const menuCart: HTMLBodyElement | null = document.querySelector("[rel='js-menu-cart-button']");
-     
-        menuButton?.addEventListener('click', handleClickMenuMobile);
-        menuCart?.addEventListener('click', handleClickMenuCart);
-    }
-
-    const handleClickMenuMobile = (): void => {
-        const menuDropDown: HTMLBodyElement | null = document.querySelector("[rel='js-menu-dropdown']");
-        const backdropEffect: HTMLBodyElement | null = document.querySelector("[rel='js-menu-dropdown-blur-effect']");
-
-        menuDropDown?.classList.toggle('active-menu');
-        backdropEffect?.classList.toggle('blur-menu-mobile-active');
-    }
-
-    const handleClickMenuCart = (): void => {
-        const modalCart: HTMLBodyElement | null = document.querySelector("[rel='js-menu-cart-modal']");
-
-        modalCart?.classList.toggle('cart-menu-active');
-    }
-
-    menus();
-    quantityProduct();
-});
+quantityProduct();
+mobileMenu();
+cartMenu();
